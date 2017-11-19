4c61b3c6434b64a5e050675ffedd9cd88cef822e
Brian Bycynski <high5bri@hotmail.com>
2017-11-19 08:43:03 -0500

    finished lecture 120

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Aux from '../../../hoc/Aux'
+
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+      return  <li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+              </li>
+    })
+  return (
+      <Aux>
+          <h3>Your Order</h3>
+          <p>A Delicious Burger with the following ingredients:</p>
+          <ul>
+            {ingredientSummary}
+          </ul>
+          <p>Continue To CheckOut?</p>
+      </Aux>
+  )
+}
+
+export default orderSummary
