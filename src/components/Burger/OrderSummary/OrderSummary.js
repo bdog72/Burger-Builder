da2011a0c267f794a95028433a3b6612707234ab
Brian Bycynski <high5bri@hotmail.com>
2017-11-22 08:22:13 -0500

    finished section 8 lecture 141

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Aux from '../../../hoc/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+
+class OrderSummary extends Component {
+
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate')
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return  <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
               </li>
     })
-  return (
+    return (
       <Aux>
-          <h3>Your Order</h3>
-          <p>A Delicious Burger with the following ingredients:</p>
-          <ul>
-            {ingredientSummary}
-          </ul>
-          <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-          <p>Continue To CheckOut?</p>
-          <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-          <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-      </Aux>
-  )
+      <h3>Your Order</h3>
+      <p>A Delicious Burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+      <p>Continue To CheckOut?</p>
+      <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+  </Aux>
+    )
+  }
 }
-
-export default orderSummary
+export default OrderSummary
