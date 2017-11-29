69bcfa9936176681a99dc4d0f83a7a84dba80714
Brian Bycynski <high5bri@hotmail.com>
2017-11-29 08:55:32 -0500

    finished lecture 232

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -31,7 +32,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -45,7 +47,8 @@ class ContactData extends Component {
                     minLength: 5,
                     maxLength: 5
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -57,7 +60,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -69,7 +73,8 @@ class ContactData extends Component {
                 validation: {
                     required: true
                 },
-                valid: false
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -79,9 +84,12 @@ class ContactData extends Component {
                         { value: 'cheapest', displayValue: 'Cheapest' }
                     ]
                 },
-                value: ''
+                value: '',
+                validation: {},
+                valid: true
             }
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -111,15 +119,18 @@ class ContactData extends Component {
     }
 
     checkValidity(value, rules) {
-        let isValid = false
+        let isValid = true
+        if (!rules) {
+            return true
+        }
         if (rules.required) {
-            isValid = value.trim() !== ''
+            isValid = value.trim() !== '' && isValid
         }
         if (rules.minLength) {
-            isValid = value.length >= rules.minLength
+            isValid = value.length >= rules.minLength && isValid
         }
         if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength
+            isValid = value.length <= rules.maxLength && isValid
         }
         return isValid
     }
@@ -136,9 +147,15 @@ class ContactData extends Component {
             updatedFormElement.value,
             updatedFormElement.validation
         )
-        console.log(updatedFormElement)
+        updatedFormElement.touched = true
         updatedOrderForm[inputIdentifier] = updatedFormElement
-        this.setState({ orderForm: updatedOrderForm })
+
+        let formIsValid = true
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+        }
+
+        this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid })
     }
 
     render() {
@@ -157,20 +174,25 @@ class ContactData extends Component {
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
                         changed={event =>
                             this.inputChangedHandler(event, formElement.id)
                         }
                     />
                 ))}
 
-                <Button btnType="Success" clicked={this.orderHandler}>
+                <Button btnType="Success" disabled={!this.state.formIsValid}>
                     ORDER
                 </Button>
             </form>
         )
+
         if (this.state.loading) {
             form = <Spinner />
         }
+
         return (
             <div className={classes.ContactData}>
                 <h4>Enter Your Contact Data</h4>
