c5413c1473ca938dd188dcce6f7cd3468fea23ab
Brian Bycynski <high5bri@hotmail.com>
2017-12-01 11:03:18 -0500

    finished section 262

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
 import axios from '../../../axios-orders'
 import Button from '../../../components/UI/Button/Button'
 
@@ -202,4 +204,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData)
